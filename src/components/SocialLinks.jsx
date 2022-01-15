fd92b06594ec1fe5e40625cfdcd0709f7b9bec4c
Thierry <marcelinthierry@gmail.com>
2022-01-16 02:27:21 +0800

    Updated Projects

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,73 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import {Button, NavItem, NavLink} from "reactstrap";
+import { Button, NavItem, NavLink } from "reactstrap";
 
 import { socialLinks } from "../portfolio";
 
 const SocialLinks = () => {
-    return ( 
-        <div className="btn-wrapper text-lg">
-            <Button
-                  className="btn-icon-only rounded-circle"
-                  color="facebook"
-                  href={socialLinks.twitter}
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-twitter" />
-                  </span>
-                </Button>
-                {/*<Button*/}
-                {/*  className="btn-icon-only rounded-circle ml-1"*/}
-                {/*  color="facebook"*/}
-                {/*  href={socialLinks.facebook}*/}
-                {/*  target="_blank"*/}
-                {/*>*/}
-                {/*  <span className="btn-inner--icon">*/}
-                {/*    <i className="fa fa-facebook-square" />*/}
-                {/*  </span>*/}
-                {/*</Button>*/}
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="facebook"
-                  href={socialLinks.github}
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-github" />
-                  </span>
-                </Button>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="facebook"
-                  href={socialLinks.linkedin}
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-linkedin" />
-                  </span>
-                </Button>
+  return (
+    <div className="btn-wrapper text-lg">
+      <Button
+        className="btn-icon-only rounded-circle"
+        color="facebook"
+        href={socialLinks.twitter}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-twitter" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="facebook"
+        href={socialLinks.github}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-github" />
+        </span>
+      </Button>
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="facebook"
+        href={socialLinks.linkedin}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-linkedin" />
+        </span>
+      </Button>
 
-            <Button
-                className="btn-icon-only rounded-circle ml-1"
-                color="facebook"
-                href={socialLinks.email}
-                target="_blank">
-                 <span className="btn-inner--icon">
-                     <i className="fa fa-envelope" />
-                 </span>
-            </Button>
-            <Button
-                className="btn-icon-only rounded-circle ml-1"
-                color="facebook"
-                href={socialLinks.instagram}
-                target="_blank">
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-instagram" />
-                  </span>
-            </Button>
-        </div>
-     );
-}
- 
+      <Button
+        className="btn-icon-only rounded-circle ml-1"
+        color="facebook"
+        href={socialLinks.email}
+        target="_blank"
+      >
+        <span className="btn-inner--icon">
+          <i className="fa fa-envelope" />
+        </span>
+      </Button>
+    </div>
+  );
+};
+
 export default SocialLinks;
